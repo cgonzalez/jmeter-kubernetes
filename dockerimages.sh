d1c1f06213fc7775c42f9aab4ca1921b14b4ec88
cgonzalez <chgonzalezg@gmail.com>
2021-02-08 13:08:54 -0300

    Update dockerimages.sh

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag="cgonzalez/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="cgonzalez/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="cgonzalez/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="cgonzalez/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="chgonzalez/jmeter-base:latest" -f Dockerfile-base .
+docker build --tag="chgonzalez/jmeter-master:latest" -f Dockerfile-master .
+docker build --tag="chgonzalez/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="chgonzalez/jmeter-reporter" -f Dockerfile-reporter .
